4d61514024786335f281cd033c02b463712a6a0b
Javi Mogan <javimogan@MacBook-Pro-de-Javi.local>
2023-03-22 19:06:04 +0100

    v 1.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { BRANDS } from './brands';
 import { EDefaultTypes, IBrand } from './types/brand';
-import { autoCompleteBrand, getBrandByName as _getBrandByName } from './utils/getBrandByName';
+import { getBrandByName as _getBrandByName } from './utils/getBrandByName';
 
 export function getBrandByName(name: string, model?: string | undefined, type?: EDefaultTypes): IBrand | null {
     return _getBrandByName(name, model) ?? BRANDS.find(brand => brand.slug === `default-${type}`) ?? null;
