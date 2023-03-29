6673f186ede1211a9ffea858305f98cdf5036542
javimogan <diazalonsojavi@gmail.com>
2023-03-29 18:05:27 +0200

    v1.1

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,5 @@ import moment from 'moment';
 export function getBrandByName(name: string, model: string = "", type: EDefaultTypes = EDefaultTypes.CAR, year: number = moment().get('years')): IBrand | null {
     return _getBrandByName(name, model) ?? BRANDS.find(brand => brand.slug === `default-${type}`) ?? null;
 }
-
 /* Get all brands */
 export const brands = BRANDS;
